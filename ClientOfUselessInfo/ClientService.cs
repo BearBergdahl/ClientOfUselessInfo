26dc9339429b92023bf5388646876c92bf0781cd
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-20 14:00:17 +0100

    Add project files.

---
+++ ClientOfUselessInfo/ClientService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClientOfUselessInfo
+{
+    
+    internal static class ClientService
+    {
+        private static HttpClient GetHttpClient()
+        {
+            HttpClient client = new HttpClient();
+            return client;
+        }
+
+        internal static async Task<HttpResponseMessage> GetData(string path)
+        {
+            using (HttpClient client = GetHttpClient())
+            {
+                return await client.GetAsync(path);
+            }            
+        }  
+    }
+}
---
+++ ClientOfUselessInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClientOfUselessInfo
+{
+    internal class Program
+    {
+        private const string UriString = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+
+        static async Task Main(string[] args)
+        {
+            var response = await ClientService.GetData(UriString);
+            string json = await response.Content.ReadAsStringAsync();
+                              
+        }
+    }
+}
